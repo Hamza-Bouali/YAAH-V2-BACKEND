bd993a47aa3ef81dc447de8c4a6b41d57856b085
Hamza-Bouali <hamza.bouali@esi.ac.ma>
2025-01-14 16:11:34 +0100

    new

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$_4!xgs50f-ky206_m*di3$xo&n1*xhk96hi+(0nt6h4=q)v&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
